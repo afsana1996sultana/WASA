b8e83bf8f3b3239610e69bbf1fc01fbf7c5529f5
Afsana Sultana <Afsana Sultana@DESKTOP-QBFDN9A>
2021-08-30 22:25:44 +0600

    Add project files.

---
+++ WASA/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WASA.Models;
+
+namespace WASA.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProcessLogin(UserModel userModel)
+        {
+            if (userModel.UserName == "Afsana" && userModel.password == "1234")
+            {
+                return View("Dashboard", userModel);
+            } else
+            {
+                return View("LoginFailure", userModel);
+            }
+           
+        }
+
+    }
+}
