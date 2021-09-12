c4c7e87addb514b3c0fe8c8b02c5e0347f00e5a5
Afsana Sultana <Afsana Sultana@DESKTOP-QBFDN9A>
2021-09-12 17:00:02 +0600

    ReportController.cs GetWasaReport.cshtml

--- WASA/Controllers/ReportController.cs
+++ WASA/Controllers/ReportController.cs
@@ -8,11 +8,16 @@ namespace WASA.Controllers
 {
     public class ReportController : Controller
     {
+
+        //private string id;
+        //private object lr;
+
         [HttpGet]
         public IActionResult GetAll()
         {
             return View();
         }
+
 
         [HttpGet]
         public IActionResult GetWasaReport()
@@ -68,11 +73,45 @@ namespace WASA.Controllers
             ViewData["WasaReport"] = rportlist;
 
             return View();
-
         }
-
     }
 }
+
+        //    string reportType = id;
+        //    string mimeType;
+        //    string encoding;
+        //    string fileNameExtension;
+
+        //    string deviceInfo =
+        //        "<DeviceInfo>" +
+        //        "<OutputFormat>" + id + "</OutputFormat>" +
+        //        "<PageWidth> 8.15in</PageWidth>" +
+        //        "<PageHeight>11in</PageHeight>" +
+        //        "<MarginTop>0.5in</MarginTop>" +
+        //        "<MarginLeft>1in</MarginLeft>" +
+        //        "<MarginRight>1in</MarginRight>" +
+        //        "<MarginBottom>0.5in</MarginBottom>" +
+        //        "</DeviceInfo>";
+        //    WasaReportModel[] wasa;
+        //    string[] streams;
+        //    byte[] renderedBytes;
+        //    renderedBytes = lr.Render(
+        //        formet: reportType,
+        //        deviceInfo,
+        //        out mimeType,
+        //        out encoding,
+        //        out fileNameExtension,
+        //        out streams,
+        //        out wasa);//byte[]
+        //    return File(renderedBytes, mimeType);
+
+        //}   
+                
+
+        
+
+    
+
            
             
 
