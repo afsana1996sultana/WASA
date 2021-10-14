3c65014d0ed35b26ab83202bb0e785806f301e68
Afsana Sultana <Afsana Sultana@DESKTOP-QBFDN9A>
2021-10-14 16:34:28 +0600

    GetAllPump.cshtml Pump1.cshtml Pump2.cshtml Pump3.cshtml Pump4.cshtml Pump5.cshtml Pump6.cshtml Pump7.cshtml Pump8.cshtml Pump9.cshtml Pump10.cshtml Pump11.cshtml BarChart.js

---
+++ WASA/wwwroot/js/BarChart.js
@@ -0,0 +1,44 @@
+﻿var barchart = document.getElementById('bar').getContext('2d');
+var mybarchart = new chart(barchart, {
+    type: 'bar',
+    data: {
+        labels: ['001', '002', '003'],
+        datasets: [{
+            label: 'Bar Chart',
+            data: [100, 200, 300],
+            backgroundColor: 'rgba(6,128,249)'
+        }]
+    },
+    Options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+})
+
+
+var piechart = document.getElementById('pie').getContext('2d');
+var mypiechart = new chart(piechart, {
+    type: 'pie',
+    data: {
+        labels: ['001', '002', '003'],
+        datasets: [{
+            label: 'Pie Chart',
+            data: [100, 200, 300],
+            backgroundColor: 'rgba(6,128,249)'
+        }]
+    },
+    Options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+})
